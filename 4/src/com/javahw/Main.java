b8ee39aa769f474a01b387ce3ac04a0f2b100def
artemZborovskiyJava <artem_zborovskiy@icloud.com>
2021-03-14 17:21:52 +0200

    fourth task fix

--- 4/src/com/javahw/Main.java
+++ 4/src/com/javahw/Main.java
@@ -3,6 +3,7 @@ package com.javahw;
 public class Main {
 
     public static final int MAX_AREA = 100;
+    public static final int MAX_SIDE = 200;
 
     public static void main(String[] args) {
 	    /*
@@ -12,55 +13,23 @@ public class Main {
 	    */
 
         int count = 0;
-        int maxSideLength = getMaxSideLength(MAX_AREA);
-        double area = 0;
-        for(int a = 1; a <= maxSideLength && area <= MAX_AREA; a++) {
-            for(int b = a; b <= maxSideLength && area <= MAX_AREA; b++) {
-                for(int c = b; c <= maxSideLength && area <= MAX_AREA; c++) {
-                    if(isThereTriangle(a, b, c) && area <= MAX_AREA) {
-                        area = getArea(a, b, c);
-                        if(area <= MAX_AREA) {
+        for (int a = 1; a <= MAX_SIDE; a++) {
+            for (int b = a; b <= MAX_SIDE; b++) {
+                for (int c = b; c <= MAX_SIDE; c++) {
+                    if (a + b > c) {
+                        double square = findArea(a, b, c);
+                        if (square <= MAX_AREA) {
                             count++;
                         }
                     }
-                }
-                if (area > MAX_AREA) {
-                    area = 0;
                 }
             }
         }
         System.out.println("count -> " + count);
     }
 
-    private static int getMaxSideLength(int maxArea) {
-        int a;
-        int b;
-        int c;
-        for (a = 1, b = 1, c = 1; a + b > c; b++, c++) {
-            if (getArea(a, b, c) >= maxArea) {
-                break;
-            }
-        }
-        if (b > c) {
-            return b - 1;
-        } else {
-            return c - 1;
-        }
-    }
-
-    private static boolean isThereTriangle(int a, int b, int c) {
-        if(a + b > c) {
-            if(b + c > a) {
-                if(a + c > b) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private static double getArea(int a, int b, int c) {
-        double p = (double) (a + b + c) / 2;
+    private static double findArea(int a, int b, int c) {
+        double p = (a + b + c) / 2.0;
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 }
